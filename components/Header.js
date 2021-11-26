e7d2392415b6e77c398f4d7cd84bdb0daea1583d
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-26 11:25:01 +0100

    refactor: Build the search Bar in Header Component

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,12 @@
 import Image from "next/image";
-
+import {
+  HeartIcon,
+  MenuIcon,
+  PaperAirplaneIcon,
+  PlusCircleIcon,
+  SearchIcon,
+  UserGroupIcon,
+} from "@heroicons/react/outline";
 function Header() {
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
@@ -11,6 +18,15 @@ function Header() {
           priority
           objectPosition="left"
         />
+      </div>
+      {/* Middle -search */}
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          type="text"
+          placeholder="Start your Search"
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+        />{" "}
+        <SearchIcon className="h-8 hidden md:inline-flex bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2 " />
       </div>
     </header>
   );
